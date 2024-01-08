4d5f09d06825b183617a510bb2232b504f0f0926
lemonlux <sololucia17@gmail.com>
2024-01-08 17:30:48 +0100

    data

---
+++ myPortfolio/src/data/CV.js
@@ -0,0 +1,87 @@
+export const CV = {
+    person: {
+      name: "Lucía",
+      surname: "Martínez Cerón",
+      city: "Murcia, España",
+      email: "sololucia17@gmail.com",
+      birthDate: "17/02/1996",
+      image: "https://res.cloudinary.com/daxddugwt/image/upload/v1700143827/Captura_de_pantalla_2023-10-11_a_las_16.47.07_rltlp8.png",
+      gitHub: "https://github.com/lemonlux/",
+      aboutMe: [
+        {
+          info: "",
+        },
+        {
+          info: "",
+        },
+        {
+          info: "",
+        },
+        {
+          info: "",
+        },
+      ],
+    },
+    education: [
+      {
+        name: "Bootcamp Full Stack Developer",
+        id: 3,
+        date: "sept 2023 - dic 2023",
+        where: "NEOLAND",
+        description:
+          ""
+      },
+      {
+        name: "Máster en Industria Cosmética",
+        id: 2,
+        date: "feb 2019 - jun 2020",
+        where: "Colegio Químicos de Murcia",
+        description:
+          ""
+      },
+      {
+        name: "Grado en Química",
+        id: 1,
+        date: "2014 - 2019",
+        where: "Universidad de Murcia",
+        description:
+        "Alumna interna durante 2 años, desarrollando un proyecto de Química Computacional sobre el que más tarde basé mi TFG"
+      },
+    ],
+    experience: [
+      {
+        name: "Química en laboratorio cromatográfico",
+        date: "nov 2019 - nov 2023",
+        where: "Iberchem",
+        description:
+          "Análisis químico, control de calidad. SAP, AMDIS y Agilent Data Analysis.",
+      }
+    ],
+    languages: [
+        {
+         language: "Español",
+        level: "Nativo",
+        },
+        {
+      language: "Inglés",
+      level: "C1",
+
+    }],
+    hardSkills: [
+      "HTML5",
+      "CSS3",
+      "Javascript",
+      "React",
+      "Styled Components",
+      "Node js",
+      "Insomnia",
+      "MongoDB"
+    ],
+    softSkills: [
+        "Creatividad",
+        "Trabajo en equipo",
+        "Organización",
+        "Resolución de problemas",
+    ]
+  
+  };
