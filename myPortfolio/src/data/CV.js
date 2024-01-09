2937d34e3a1998974bf8e8c7c2fc01d320d7c880
lemonlux <sololucia17@gmail.com>
2024-01-09 17:21:51 +0100

    commit

--- myPortfolio/src/data/CV.js
+++ myPortfolio/src/data/CV.js
@@ -4,22 +4,15 @@ export const CV = {
       surname: "Martínez Cerón",
       city: "Murcia, España",
       email: "sololucia17@gmail.com",
+      
       birthDate: "17/02/1996",
       image: "https://res.cloudinary.com/daxddugwt/image/upload/v1700143827/Captura_de_pantalla_2023-10-11_a_las_16.47.07_rltlp8.png",
       gitHub: "https://github.com/lemonlux/",
+      linkedin:"https://www.linkedin.com/in/luc%C3%ADa-mart%C3%ADnez-cer%C3%B3n-7326b2185/",
       aboutMe: [
-        {
-          info: "",
-        },
-        {
-          info: "",
-        },
-        {
-          info: "",
-        },
-        {
-          info: "",
-        },
+         "Junior FullStack Developer, desarrollando proyectos Freelance desde que terminé el Bootcamp de Neoland.",
+         "Graduada en Química con un Máster en Ciencia e Industria Cosmética, especializada en química computacional durante la carrera.",
+         "Buscando una oportunidad laboral para seguir aprendiendo y mejorar en el ámbito de la programación"
       ],
     },
     education: [
@@ -29,7 +22,7 @@ export const CV = {
         date: "sept 2023 - dic 2023",
         where: "NEOLAND",
         description:
-          ""
+          "Curso intensivo de desarrollo web en el que adquirí conocimientos de Backend y Frontend, sobre los lenguajes Javascript, HTML y CSS, y tecnologías como React, Node.js, MongoDB, Styled Components, Vite, GitHub, Axios,... "
       },
       {
         name: "Máster en Industria Cosmética",
@@ -54,8 +47,48 @@ export const CV = {
         date: "nov 2019 - nov 2023",
         where: "Iberchem",
         description:
-          "Análisis químico, control de calidad. SAP, AMDIS y Agilent Data Analysis.",
+          "Análisis químico, cromatografía de gases, control de calidad. SAP, AMDIS y Agilent Data Analysis.",
+      },
+      {
+        name: "Freelance",
+        date: "sept 2023 - actualidad",
+        where: "",
+        description:
+          "Desde el inicio del Bootcamp de Web Development, he desarrollado diversos proyectos tanto por mi cuenta como en equipo con el resto de alumnos del curso.",
       }
+    ],
+    projects: [
+      {
+        name: "Connect-a-mate",
+        date: "dic 2023",
+        images: ["", ""],
+        description:
+          "Proyecto final de bootcamp que realicé en equipo junto a mis compañeros de curso. Se trata de una red social para conectar y buscar personas afines a ti a la hora de compartir piso. Cuenta con autenticación de usuario y personalización del perfil, publicaciones en un feed, reseñas a los usuarios, chat privado, así como los anuncios de cada habitación disponible.",
+        technologies: "Javascript, Node.js, MongoDB, React, Styled Components",
+        url: "",
+        gitHub: "",
+      },
+      {
+        name: "Sports",
+        date: "nov 2023",
+        images: ["", ""],
+        description:
+          'Proyecto de React + Node.js que realicé con mis compañeros. Es una página de varios deportes en la que practicamos todos los conceptos aprendidos de React, utilizando Node.js para las llamadas a la base de datos, y sobre la que cabe destacar la posibilidad de los usuarios de crear sus "podios" de motogp y "eleven" de fútbol favoritos.',
+        technologies: "Javascript, CSS, Node.js, MongoDB, React",
+        url: "https://fifa-sports.vercel.app/",
+        frontEnd:  "https://github.com/eolimacion/reactFrontend",
+        backEnd: "https://github.com/eolimacion/reactBackend",
+      },
+      {
+        name: "Pokedex",
+        date: "oct 2023",
+        images: ["", ""],
+        description:
+          "Proyecto individual de Javascript en el que realicé el fetch a una API para mostrar 150 pokemons, y en la que además incluí dos juegos programados con Javascript.",
+        technologies: "Javascript, CSS, HTML",
+        url: "",
+        gitHub: "",
+      },
     ],
     languages: [
         {
@@ -73,7 +106,7 @@ export const CV = {
       "Javascript",
       "React",
       "Styled Components",
-      "Node js",
+      "Nodejs",
       "Insomnia",
       "MongoDB"
     ],
@@ -81,7 +114,12 @@ export const CV = {
         "Creatividad",
         "Trabajo en equipo",
         "Organización",
+        "Análisis",
+        "Iniciativa",
+        "Adaptación",
         "Resolución de problemas",
+        "Empatía",
+        "Comunicación y escucha"
     ]
   
   };
